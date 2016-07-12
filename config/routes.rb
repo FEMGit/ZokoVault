856b7abeee79eb3f9a9f94c64910521dd53a2c66
Tim Ferro <timferro@gmail.com>
2016-07-11 20:52:33 -0500

    user cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'files' => 'welcome#files'
   get 'styleguide' => 'welcome#styleguide'
 
+  resources :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 

640d4b02d117490ec2df33973d607da40029e86d
Dylan <conleydylan@gmail.com>
2016-07-31 08:53:20 -0500

    Removing scaffold-generated view test that requires column represent only a single resource. Bogus.

--- spec/views/contacts/index.html.erb_spec.rb
+++ spec/views/contacts/index.html.erb_spec.rb
@@ -46,25 +46,5 @@ RSpec.describe "contacts/index", type: :view do
     ])
   end
 
-  it "renders a list of contacts" do
-    render
-    assert_select "tr>td", :text => "Firstname".to_s, :count => 2
-    assert_select "tr>td", :text => "Lastname".to_s, :count => 2
-    assert_select "tr>td", :text => "Emailaddress".to_s, :count => 2
-    assert_select "tr>td", :text => "Phone".to_s, :count => 2
-    assert_select "tr>td", :text => "Category".to_s, :count => 2
-    assert_select "tr>td", :text => "Relationship".to_s, :count => 2
-    assert_select "tr>td", :text => "Beneficiarytype".to_s, :count => 2
-    assert_select "tr>td", :text => "Ssn".to_s, :count => 2
-    assert_select "tr>td", :text => "Address".to_s, :count => 2
-    assert_select "tr>td", :text => "Zipcode".to_s, :count => 2
-    assert_select "tr>td", :text => "State".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Avatarcolor".to_s, :count => 2
-    assert_select "tr>td", :text => "Photourl".to_s, :count => 2
-    assert_select "tr>td", :text => "Businessname".to_s, :count => 2
-    assert_select "tr>td", :text => "Businesswebaddress".to_s, :count => 2
-    assert_select "tr>td", :text => "Businessphone".to_s, :count => 2
-    assert_select "tr>td", :text => "Businessfax".to_s, :count => 2
-  end
+  
 end

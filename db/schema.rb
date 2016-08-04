1cd78c1670f26b33422b0e33821995a7169f10da
Dylan <conleydylan@gmail.com>
2016-08-03 23:14:17 -0500

    Document has category, this and folder on edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160730024041) do
+ActiveRecord::Schema.define(version: 20160804033536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160730024041) do
     t.string   "url",         null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
   add_index "documents", ["folder_id"], name: "index_documents_on_folder_id", using: :btree

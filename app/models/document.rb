d31e4c2be39495f40860f4a815a9fb6f1d3d8b48
Dylan <conleydylan@gmail.com>
2016-08-07 14:26:31 -0500

    Shares for document pulled in dynamically

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,4 +5,6 @@ class Document < ActiveRecord::Base
   scope :for_user, ->(user) {where(user: user)}
 
   scope :in_folder, ->(folder) {where(folder: folder)}
+  has_many :shares, foreign_key: "document_id" 
+  has_many :contacts, through: :shares
 end

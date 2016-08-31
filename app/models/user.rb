4648704d20d14bdab7ca1ea321712e8fe17a0d1c
Ted Price <ted.price@gmail.com>
2016-08-30 21:52:34 -0500

    Cosmetic Updates for Vendors, Contacts, and Documents
    * Remove destroy links in the index pages
    * Add 'delete' links to the edit view
    * Modify the Contact 'edit' view to be wizard
    * Contact#name helper method
    * Updated vendor index to use contact name and user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :lockable, :registerable,
          :recoverable, :timeoutable, :trackable, :validatable
+
 end

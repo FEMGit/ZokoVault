a612f0996126c4a8d2e2189b4076fd0407bd5550
rzudin <r.zudin@velvetech.ru>
2016-09-15 22:39:30 -0400

    update seeds method

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,17 @@
 
 emails = %w[ admin@zokuvault.com user@zokuvault.com user@example.com ]
 
-# users = emails.map do |email|
-#   user = User.find_or_initialize_by(email: email)
-#   user.password = user.password_confirmation = "password"
-#   user.confirmed_at = Time.now
-#   user.save
-#   user
-# end
+ users = emails.map do |email|
+   user = User.find_or_initialize_by(email: email)
+   user.password = user.password_confirmation = "password"
+   user.confirmed_at = Time.now
+   user.save
+   user
+ end
 
-# #
-# # Create a few contacts for each user
-# #
-# categories = %w[work sports social family religious in-law]
+ #
+ # Create a few contacts for each user
+ #
 
 user_contacts = users.map do |user|
   contacts = 10.times.map do 
@@ -32,13 +31,13 @@ user_contacts = users.map do |user|
     )
   end
 
-#   [user, contacts]
-# end
+   [user, contacts]
+ end
 
-# #
-# # Create a vendor per contact
-# #
-# groups = %w[life property health]
+ #
+ # Create a vendor per contact
+ #
+ groups = %w[life property health]
 
 user_contacts.each do |user, contacts|
   contacts.each do |contact|

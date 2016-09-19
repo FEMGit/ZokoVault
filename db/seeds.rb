b530823339ce775e0079b9a211fbe9d916acfa94
rzudin <r.zudin@velvetech.ru>
2016-09-19 07:02:09 -0400

    Merge conflict resolve

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
 # Create users
+<<<<<<< 5359d8f6e79ea63dcf961cde872ddd7562b108e8
+
+emails = %w[ admin@zokuvault.com user@zokuvault.com user@example.com ]
+=======
+>>>>>>> Merge conflict resolve
 
 emails = %w[ admin@zokuvault.com user@zokuvault.com user@example.com ]
 
- users = emails.map do |email|
-   user = User.find_or_initialize_by(email: email)
-   user.password = user.password_confirmation = "password"
-   user.confirmed_at = Time.now
-   user.save
-   user
- end
-
- #
- # Create a few contacts for each user
- #
+users = emails.map do |email|
+  user = User.find_or_initialize_by(email: email)
+  user.password = user.password_confirmation = "password"
+  user.confirmed_at = Time.now
+  user.save
+  user
+end
 
 user_contacts = users.map do |user|
   contacts = 10.times.map do 
@@ -31,14 +32,32 @@ user_contacts = users.map do |user|
     )
   end
 
-   [user, contacts]
- end
+user_contacts = users.map do |user|
+  contacts = 10.times.map do 
+    contact_type = Contact::CONTACT_TYPES.keys.sample
+    relationship = Contact::CONTACT_TYPES[contact_type].sample
 
- #
- # Create a vendor per contact
- #
- groups = %w[life property health]
+    Contact.find_or_create_by(
+      firstname: Faker::Name.first_name,
+      lastname: Faker::Name.last_name,
+      emailaddress: Faker::Internet.free_email,
+      phone: Faker::PhoneNumber.phone_number,
+      birthdate: Faker::Date.between(60.years.ago, 20.years.ago),
+      user: user,
+      contact_type: contact_type,
+      relationship: relationship
+    )
+  end
 
+  [user, contacts]
+end
+
+#
+# Create a vendor per contact
+#
+groups = %w[life property health]
+
+>>>>>>> Merge conflict resolve
 user_contacts.each do |user, contacts|
   contacts.each do |contact|
     Vendor.create!(

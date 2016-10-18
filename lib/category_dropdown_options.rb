00fd052de4390a95f61fcec09b456e451ff29a88
vvtzv <zokuvault@velvetech.com>
2016-10-18 10:12:05 +0600

    Ad-267 Add new dropdown option to Category Select (#156)

---
+++ lib/category_dropdown_options.rb
@@ -0,0 +1,14 @@
+module CategoryDropdownOptions
+  CATEGORIES = [
+    "Select...",
+    "My Profile",
+    "Contact",
+    "Financial Information",
+    "Wills - Trusts - Legal",
+    "Insurance",
+    "Taxes",
+    "Healthcare Choices",
+    "Final Wishes",
+    "Web Accounts"
+    ]
+end

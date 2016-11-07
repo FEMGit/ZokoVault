9682e45ca879be840cd869690bc8a400b24ec3f1
rzudin <r.zudin@velvetech.ru>
2016-11-07 02:13:15 -0500

    Ad-369 Contact Us Email

---
+++ app/models/message.rb
@@ -0,0 +1,11 @@
+class Message
+  include ActiveModel::Model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+  
+  attr_accessor :name, :email, :phone_number, :message_content
+  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
   delete 'insurance/properties/provider/:id' => 'property_and_casualties#destroy_provider'
   delete 'insurance/healths/provider/:id' => 'healths#destroy_provider'
   delete 'insurance/lives/provider/:id' => 'life_and_disabilities#destroy_provider'
+  
+  # Mailer
+  post 'contact-us', to: 'messages#create'
 
   get 'files' => 'welcome#files'
   get 'filestacktest' => 'welcome#filestacktest'

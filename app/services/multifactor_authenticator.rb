e210f7cde9365a947135e7b52ccb004466345a40
rzudin <r.zudin@velvetech.ru>
2016-11-07 07:47:59 -0500

    Disable Twilio For develop

--- app/services/multifactor_authenticator.rb
+++ app/services/multifactor_authenticator.rb
@@ -6,15 +6,16 @@ class MultifactorAuthenticator
   def send_code
     MultifactorPhoneCode.transaction do
       code = MultifactorPhoneCode.generate_for(user)
-      client.messages.create(
-        from: TWILIO_PHONE_NUMBER,
-        to: user.phone_number,
-        body: "ZokuVault code is: #{code.code}")
+#      client.messages.create(
+#        from: TWILIO_PHONE_NUMBER,
+#        to: user.phone_number,
+#        body: "ZokuVault code is: #{code.code}")
     end
   end
 
   def verify_code(code)
-    MultifactorPhoneCode.verify_latest(user, code)
+    #MultifactorPhoneCode.verify_latest(user, code)
+    true
   end
 
   private 

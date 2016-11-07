69c154a63e9e373662796784fba42e02a5eb3aad
rzudin <r.zudin@velvetech.ru>
2016-11-07 02:13:15 -0500

    Ad-369 Contact Us Email

--- config/routes.rb
+++ config/routes.rb
@@ -42,7 +42,7 @@ Rails.application.routes.draw do
   # Mailer
   post 'contact-us', to: 'messages#create'
   post 'mailing-list', to: 'interested_users#create'
-  
+
   # Taxes
   get 'taxes/:id/:year', to: 'taxes#show'
   get 'taxes/new/:year', to: 'taxes#create'

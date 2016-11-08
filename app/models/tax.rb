d6b83f00b7cf3d9be396300bbb75de50d15957eb
rzudin <r.zudin@velvetech.ru>
2016-11-08 04:38:30 -0500

    Ad-381 - add new taxes form

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,9 +1,11 @@
 class Tax < ActiveRecord::Base
   scope :for_user, ->(user) { where(user: user) }
+
   belongs_to :user
   belongs_to :document
   belongs_to :tax_preparer, class_name: "Contact"
 
   has_many :shares, as: :shareable, dependent: :destroy
+
   has_many :share_with_contacts, through: :shares, source: :contact
 end

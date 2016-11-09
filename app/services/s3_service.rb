8d2b065c4c53b314a9fd620914433a3464c18699
rzudin <r.zudin@velvetech.ru>
2016-11-09 06:21:15 -0500

    Ad-387 rubocop style fix

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -1,6 +1,6 @@
 class S3Service
   def self.get_object_by_key(document_key)
     creds = Aws::Credentials.new(Rails.application.secrets.aws_access_key_id, Rails.application.secrets.aws_access_secret_key)
-    s3object = Aws::S3::Object.new(Rails.application.secrets.aws_bucket_name, document_key, region: Rails.application.secrets.aws_region, credentials: creds)
+    Aws::S3::Object.new(Rails.application.secrets.aws_bucket_name, document_key, region: Rails.application.secrets.aws_region, credentials: creds)
   end
 end

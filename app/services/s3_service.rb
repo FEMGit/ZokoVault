b7dc8ee8b9243d5f704c166f537148a8a4ddfc80
rzudin <r.zudin@velvetech.ru>
2016-11-09 08:26:20 -0500

    Ad-388 Delete document from s3 storage

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -2,7 +2,7 @@ class S3Service
   def self.get_object_by_key(document_key)
     Aws::S3::Object.new(Rails.application.secrets.aws_bucket_name, document_key, region: Rails.application.secrets.aws_region, credentials: credentials)
   end
-
+  
   def self.delete_from_storage(document_key, options = {})
     client = Aws::S3::Client.new(region: Rails.application.secrets.aws_region, credentials: credentials)
     client.delete_object(options.merge(:bucket => Rails.application.secrets.aws_bucket_name, :key => document_key))

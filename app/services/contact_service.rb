1f10fbba43273764ef3e43bb62f72163af5b5ca6
rzudin <r.zudin@velvetech.ru>
2016-11-11 00:19:24 -0500

    Ad-406 Add delete functionality to Taxes

--- app/services/contact_service.rb
+++ app/services/contact_service.rb
@@ -1,6 +1,6 @@
 class ContactService
   attr_reader :contacts
-  
+
   def initialize(params)
     @user = params[:user]
     set_contacts
@@ -9,9 +9,9 @@ class ContactService
   def contacts_shareable
     @contacts.reject { |c| c.emailaddress == @user.email }
   end
-  
+
   private
-  
+
   def set_contacts
     @contacts = Contact.for_user(@user)
   end

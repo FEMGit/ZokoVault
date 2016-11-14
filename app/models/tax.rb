1252777a9aa5db5c5443dab94059557d5d8b380c
vvtzv <zokuvault@velvetech.com>
2016-11-13 23:45:36 -0500

    Taxes (#321)
    * Ad-379 - details tax card
    
    * Ad-380 Taxes Page Clean Up
    
    * Ad-381 - add new taxes form
    
    * Ad-403 Save Edit Dynamics on taxes form
    
    * Ad-405 - Add Taxes subcategory and document count on each of tax year

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -6,5 +6,6 @@ class Tax < ActiveRecord::Base
   belongs_to :tax_preparer, class_name: "Contact"
 
   has_many :shares, as: :shareable, dependent: :destroy
+
   has_many :share_with_contacts, through: :shares, source: :contact
 end

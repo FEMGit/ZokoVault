41529f03a8d55e8c9674e958bad0646b50d952e1
rzudin <r.zudin@velvetech.ru>
2016-11-15 21:20:05 -0500

    Ad-335 Birthdate formate fix

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -3,7 +3,7 @@ module ContactsHelper
     !user_profile.street_address_1.blank? && !user_profile.city.blank? &&
       !user_profile.state.blank?
   end
-  
+
   def show_web_address(web_address)
     if web_address.blank? || web_address.nil?
       ""

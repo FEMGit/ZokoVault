8c71da930d41d8e53ea629e82f1196fb2b77f0f2
rzudin <r.zudin@velvetech.ru>
2016-11-21 06:05:16 -0500

    Ad-436 Add dynamic to final wishes pages

--- app/controllers/final_wishes_controller.rb
+++ app/controllers/final_wishes_controller.rb
@@ -1,10 +1,13 @@
 class FinalWishesController < AuthenticatedController
+  before_action :set_final_wish_info, only: [:show, :edit, :update]
+  before_action :set_final_wish, only: [:destroy]
   before_action :set_category_and_group, :set_all_documents, only: [:index, :show, :edit]
   before_action :set_contacts, only: [:new, :edit]
 
   # GET /final_wishes
   # GET /final_wishes.json
   def index
+    @final_wishes = FinalWishInfo.for_user(current_user)
     session[:ret_url] = final_wishes_path
   end
 
@@ -18,6 +21,9 @@ class FinalWishesController < AuthenticatedController
 
   # GET /final_wishes/new
   def new
+    group = params[:group]
+    final_wish = FinalWishService.get_wish_info(group, current_user)
+    redirect_to "#{final_wishes_path}/#{final_wish[:id]}/edit" if final_wish
     @final_wish_info = FinalWishInfo.new
     @final_wish_info[:group] = params[:group]
     @final_wish_info.final_wishes << FinalWish.new
@@ -25,23 +31,23 @@ class FinalWishesController < AuthenticatedController
 
   # GET /final_wishes/1/edit
   def edit
-    @final_wish_info = FinalWishInfo.new
-    @final_wish_info[:group] = @groups[params[:id].to_i]["label"]
-    @final_wish_info.final_wishes << FinalWish.new
+    @final_wish_info = @final_wish
   end
 
   # POST /final_wishes
   # POST /final_wishes.json
   def create
-    @final_wish = FinalWish.new(final_wish_params)
-
+    final_wishes = final_wish_form_params
+    final_wishes.keys.each { |x| params[:final_wish_info].delete(x) }
+    @final_wish_info = FinalWishInfo.new(final_wish_params.merge(user_id: current_user.id))
+    FinalWishService.fill_wishes(final_wishes, @final_wish_info, current_user.id)
     respond_to do |format|
-      if @final_wish.save
-        format.html { redirect_to @final_wish, notice: 'Final wish was successfully created.' }
-        format.json { render :show, status: :created, location: @final_wish }
+      if @final_wish_info.save
+        format.html { redirect_to session[:ret_url] || final_wishes_path, notice: 'Final wish was successfully created.' }
+        format.json { render :show, status: :created, location: @final_wish_info }
       else
         format.html { render :new }
-        format.json { render json: @final_wish.errors, status: :unprocessable_entity }
+        format.json { render json: @final_wish_info.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -49,13 +55,17 @@ class FinalWishesController < AuthenticatedController
   # PATCH/PUT /final_wishes/1
   # PATCH/PUT /final_wishes/1.json
   def update
+    final_wishes = final_wish_form_params
+    final_wishes.keys.each { |x| params[:final_wish_info].delete(x) }
+    @final_wish_info = @final_wish
+    FinalWishService.fill_wishes(final_wishes, @final_wish_info, current_user.id)
     respond_to do |format|
-      if @final_wish.update(final_wish_params)
-        format.html { redirect_to @final_wish, notice: 'Final wish was successfully updated.' }
-        format.json { render :show, status: :ok, location: @final_wish }
+      if @final_wish_info.update(final_wish_params)
+        format.html { redirect_to session[:ret_url] || final_wishes_path, notice: 'Final wish was successfully updated.' }
+        format.json { render :show, status: :ok, location: @final_wish_info }
       else
         format.html { render :edit }
-        format.json { render json: @final_wish.errors, status: :unprocessable_entity }
+        format.json { render json: @final_wish_info.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -65,7 +75,7 @@ class FinalWishesController < AuthenticatedController
   def destroy
     @final_wish.destroy
     respond_to do |format|
-      format.html { redirect_to final_wishes_url, notice: 'Final wish was successfully destroyed.' }
+      format.html { redirect_to :back || final_wishes_url, notice: 'Final wish was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -82,6 +92,10 @@ class FinalWishesController < AuthenticatedController
   def set_final_wish
     @final_wish = FinalWish.find(params[:id])
   end
+  
+  def set_final_wish_info
+    @final_wish = FinalWishInfo.find(params[:id])
+  end
 
   def set_category_and_group
     @category = Rails.application.config.x.FinalWishesCategory
@@ -95,6 +109,10 @@ class FinalWishesController < AuthenticatedController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def final_wish_params
-    params.require(:final_wish).permit(:document_id, :user_id, :primary_contact_id, :notes, :group)
+    params.require(:final_wish_info).permit!
+  end
+  
+  def final_wish_form_params
+    final_wish_params.select { |k, _v| k.starts_with?("final_wish_") }
   end
 end

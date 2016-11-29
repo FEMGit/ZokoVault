a42321eb5e7d4b1a430e81e92f601ae4ecef44da
rzudin <r.zudin@velvetech.ru>
2016-11-29 00:33:47 -0500

    Ad-464 - Add Avatar Upload and Show Functionality

---
+++ app/assets/javascripts/filepicker_save.js
@@ -0,0 +1,22 @@
+var saveFileUrl = function(){
+  filepicker.setKey("AU8hye5meSjiQ6l5oOxKFz");
+  filepicker.pickAndStore({
+      customSourceContainer: 'zoku-stage',
+      extensions: ['.png', '.jpg', '.PNG', '.JPG', '.jpeg', 'JPEG', '.tiff', '.TIFF', '.gif', '.GIF'],
+      conversions: ['crop'],
+      cropMax: [400, 400],
+      cropForce: true,
+      cropRatio: 1/1,
+      multiple: false
+    },
+    {
+      location: "S3",
+      storeContainer: "zoku-stage"
+    },
+    function(Blobs) {
+      Blob = Blobs[0];
+      $('#photo_url').val(Blob.key);
+      $('#image_view').attr('src', Blob.url);
+      $('#image_preview').attr('src', Blob.url);
+    });
+  };

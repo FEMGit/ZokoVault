3117e971971a7c3b740faf9202242471a27f9650
rzudin <r.zudin@velvetech.ru>
2016-12-01 06:30:01 -0500

    Ad-483 - Add avatar tooltip

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -16,29 +16,6 @@ module ContactsHelper
     form.text_field(name, class: "form-control", :data => {:mask => '99999'})
   end
   
-  def avatar_circle(contact, with_name = false)
-    if contact.photourl.present?
-      circle = image_tag(get_file_url(contact.photourl), class: "avatar-small", alt: contact.initials.to_s)
-    else
-      circle = image_tag(empty_image, class: "avatar-small", alt: contact.initials.to_s)
-      p = content_tag(:p, contact.initials.to_s, class: "avatar-initials")
-      circle.concat(p)
-    end
-    tooltip = content_tag(:div, user_tooltip_info(contact), class: "tooltip")
-    tooltip_item = content_tag(:div, circle.concat(tooltip), class: "tooltip-item top")
-    return tooltip_item unless with_name
-    tooltip_item.concat(content_tag(:span, " #{contact.name}", class: "avatar-name"))
-  end
-  
-  private
-  
-  def user_tooltip_info(contact)
-    name = content_tag(:p, contact.name.to_s)
-    email = mail_to(contact.emailaddress.to_s, contact.emailaddress.to_s, class: "no-underline-link")
-    phone = content_tag(:p, contact.phone.to_s)
-    name.concat(email).concat(phone)
-  end
-  
   def empty_image
     asset_url('blank.gif')
   end

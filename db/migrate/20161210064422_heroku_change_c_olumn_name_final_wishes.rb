eefc9768575607ea0f9aa56b6f580e3abe3ca36f
rzudin <r.zudin@velvetech.ru>
2016-12-10 02:07:42 -0500

    AD-517 delete migration

--- db/migrate/20161210064422_heroku_change_c_olumn_name_final_wishes.rb
+++
@@ -1,6 +0,0 @@
-class HerokuChangeCOlumnNameFinalWishes < ActiveRecord::Migration
-  def change
-    change_column :final_wishes, :group, :integer
-    rename_column :final_wishes, :group, :final_wish_info_id
-  end
-end

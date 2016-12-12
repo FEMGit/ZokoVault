e95b71341ab5a28d41e3e7e08e2f53fcb2031e3a
rzudin <r.zudin@velvetech.ru>
2016-12-11 22:37:18 -0500

    Ad-513 Fix Bug with photo

--- app/assets/javascripts/filepicker_save.js
+++ app/assets/javascripts/filepicker_save.js
@@ -1,4 +1,4 @@
-var saveFileUrl = function() {
+var saveFileUrl = function(option = "") {
   filepicker.setKey("AU8hye5meSjiQ6l5oOxKFz");
   filepicker.pickAndStore({
       container: 'modal',
@@ -15,24 +15,32 @@ var saveFileUrl = function() {
     },
     function(Blobs) {
       Blob = Blobs[0];
-      $('#photo_url').val(Blob.key);
-      $('#image_view').attr('src', Blob.url);
-      $('#image_preview').attr('src', Blob.url);
-      $('#new-avatar').show();
-      $('#choose-avatar-section').hide();
-      $('#preview-avatar-section').show();
-      $('#text-avatar').hide();
-      $('.remove-button').show();
+      var id_append = ""
+      if (option === "account") {
+        id_append = "_account"
+      }
+      $('#photo_url' + id_append).val(Blob.key);
+      $('#image_view' + id_append).attr('src', Blob.url);
+      $('#image_preview' + id_append).attr('src', Blob.url);
+      $('#new-avatar' + id_append).show();
+      $('#choose-avatar-section' + id_append).hide();
+      $('#preview-avatar-section' + id_append).show();
+      $('#text-avatar' + id_append).hide();
+      $('.remove-button' + id_append).show();
     });
 };
 
-var removePhoto = function() {
-  $('#photo_url').val("");
-  $('#image_view').attr('src', "");
-  $('#image_preview').attr('src', "");
-  $('#choose-avatar-section').show();
-  $('#preview-avatar-section').hide();
-  $('#new-avatar').hide();
-  $('#text-avatar').show();
-  $('.remove-button').hide();
+var removePhoto = function(option = "") {
+  var id_append = ""
+  if (option === "account") {
+    id_append = "_account"
+  }
+  $('#photo_url' + id_append).val("");
+  $('#image_view' + id_append).attr('src', "");
+  $('#image_preview' + id_append).attr('src', "");
+  $('#choose-avatar-section' + id_append).show();
+  $('#preview-avatar-section' + id_append).hide();
+  $('#new-avatar' + id_append).hide();
+  $('#text-avatar' + id_append).show();
+  $('.remove-button' + id_append).hide();
 }

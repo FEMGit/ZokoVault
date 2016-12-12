85e49bab56d6eadc2c43ef7e017a13ff22ebee49
rzudin <r.zudin@velvetech.ru>
2016-12-11 22:37:18 -0500

    Ad-513 Fix Bug with photo

--- app/assets/javascripts/filepicker_save.js
+++ app/assets/javascripts/filepicker_save.js
@@ -1,4 +1,4 @@
-var saveFileUrl = function() {
+var saveFileUrl = function(option) {
   filepicker.setKey("AU8hye5meSjiQ6l5oOxKFz");
   filepicker.pickAndStore({
       container: 'modal',
@@ -14,25 +14,27 @@ var saveFileUrl = function() {
       storeContainer: "zoku-stage"
     },
     function(Blobs) {
-      Blob = Blobs[0];
-      $('#photo_url').val(Blob.key);
-      $('#image_view').attr('src', Blob.url);
-      $('#image_preview').attr('src', Blob.url);
-      $('#new-avatar').show();
-      $('#choose-avatar-section').hide();
-      $('#preview-avatar-section').show();
-      $('#text-avatar').hide();
-      $('.remove-button').show();
+      setViewParameters(Blobs[0].key, Blobs[0].url, option)
+      $('#new-avatar' + option).show();
+      $('#choose-avatar-section' + option).hide();
+      $('#preview-avatar-section' + option).show();
+      $('#text-avatar' + option).hide();
+      $('.remove-button' + option).show();
     });
 };
 
-var removePhoto = function() {
-  $('#photo_url').val("");
-  $('#image_view').attr('src', "");
-  $('#image_preview').attr('src', "");
-  $('#choose-avatar-section').show();
-  $('#preview-avatar-section').hide();
-  $('#new-avatar').hide();
-  $('#text-avatar').show();
-  $('.remove-button').hide();
+var removePhoto = function(option) {
+  setViewParameters("", "", option)
+  $('#new-avatar' + option).hide();
+  $('#choose-avatar-section' + option).show();
+  $('#preview-avatar-section' + option).hide();
+  $('#text-avatar' + option).show();
+  $('.remove-button' + option).hide();
+}
+
+var setViewParameters = function(photo_url, image_src, option) {
+  option = option || ""
+  $('#photo_url' + option).val(photo_url);
+  $('#image_view' + option).attr('src', image_src);
+  $('#image_preview' + option).attr('src', image_src);
 }

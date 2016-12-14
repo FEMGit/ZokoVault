e28a2db389c82da1cc148ffbf9d025cebdb0a314
rzudin <r.zudin@velvetech.ru>
2016-12-13 21:17:55 -0500

    Fix shareable problem - Ad-516 Fix

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,7 +2,7 @@ class Share < ActiveRecord::Base
   belongs_to :user
   scope :for_user, ->(user) {where(user: user)}
   belongs_to :contact
-  belongs_to :shareable
+  belongs_to :shareable, polymorphic: true
 
   validates :contact_id, presence: true
 end

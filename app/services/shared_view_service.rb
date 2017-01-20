7367f8f38f9cbad89e86db98a6bc3e5b10a64e47
rzudin <r.zudin@velvetech.ru>
2017-01-20 01:07:51 -0500

    Ad-658 - Add Legal share

--- app/services/shared_view_service.rb
+++ app/services/shared_view_service.rb
@@ -3,15 +3,15 @@ class SharedViewService
     @shared_category_names_full = shares.map(&:shareable).select { |s| s.is_a? Category }.map(&:name)
     shares.map(&:shareable).each do |shareable|
       case shareable
-      when Will || Trust || PowerOfAttorney
+      when Will, Trust, PowerOfAttorney
         @shared_category_names_full |= ['Wills - Trusts - Legal']
-      when Health || PropertyAndCasualty || LifeAndDisabilities
+      when Health, PropertyAndCasualty, LifeAndDisability
         @shared_category_names_full |= ['Insurance']
       when Tax
         @shared_category_names_full |= ['Taxes']
       when FinalWish
         @shared_category_names_full |= ['Final Wishes']
-      when FinancialInformation
+      when FinancialProvider
         @shared_category_names_full |= ['Financial Information']
       end
     end

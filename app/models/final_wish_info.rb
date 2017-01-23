9a77cfeb3eddde89622e8fdf5a606d63ca871514
rzudin <r.zudin@velvetech.ru>
2017-01-23 01:41:47 -0500

    Ad-680 - Fix migrations

--- app/models/final_wish_info.rb
+++ app/models/final_wish_info.rb
@@ -2,7 +2,9 @@ class FinalWishInfo < ActiveRecord::Base
   scope :for_user, ->(user) { where(user: user) }
 
   belongs_to :user
+  belongs_to :category
   has_many :final_wishes, class_name: "FinalWish", foreign_key: :final_wish_info_id
 
   accepts_nested_attributes_for :final_wishes
+  before_save { self.category = Category.fetch("final wishes") }
 end

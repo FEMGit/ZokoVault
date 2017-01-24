4c8168e16a5741db29db7a2e24e536c2e6536eb6
rzudin <r.zudin@velvetech.ru>
2017-01-24 08:05:54 -0500

    Ad-689 - Fix Add document issue on insurance page

--- app/models/financial_alternative.rb
+++ app/models/financial_alternative.rb
@@ -14,6 +14,4 @@ class FinancialAlternative < ActiveRecord::Base
   belongs_to :category
   belongs_to :primary_contact, class_name: "Contact"
   belongs_to :owner, class_name: "Contact"
-  
-  before_save { self.category = Category.fetch("financial information") }
 end

0dbd6369b85d9ddfd76dd7a4c8dde790919f4f75
rzudin <r.zudin@velvetech.ru>
2017-01-24 23:40:44 -0500

    Ad-694 - Fix terminal error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124074414) do
+ActiveRecord::Schema.define(version: 20170125043917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,8 +131,10 @@ ActiveRecord::Schema.define(version: 20170124074414) do
     t.datetime "updated_at",                null: false
     t.integer  "user_id"
     t.integer  "account_provider_id"
+    t.integer  "category_id"
   end
 
+  add_index "financial_account_informations", ["category_id"], name: "index_financial_account_informations_on_category_id", using: :btree
   add_index "financial_account_informations", ["user_id"], name: "index_financial_account_informations_on_user_id", using: :btree
 
   create_table "financial_alternatives", force: :cascade do |t|
@@ -149,8 +151,10 @@ ActiveRecord::Schema.define(version: 20170124074414) do
     t.integer  "user_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.integer  "category_id"
   end
 
+  add_index "financial_alternatives", ["category_id"], name: "index_financial_alternatives_on_category_id", using: :btree
   add_index "financial_alternatives", ["user_id"], name: "index_financial_alternatives_on_user_id", using: :btree
 
   create_table "financial_investments", force: :cascade do |t|
@@ -329,6 +333,23 @@ ActiveRecord::Schema.define(version: 20170124074414) do
   end
 
   add_index "shares", ["user_id"], name: "index_shares_on_user_id", using: :btree
+
+  create_table "table_financial_alternatives", force: :cascade do |t|
+    t.integer  "alternative_type"
+    t.string   "name"
+    t.integer  "owner_id"
+    t.decimal  "commitment"
+    t.decimal  "total_calls"
+    t.decimal  "total_distributions"
+    t.decimal  "current_value"
+    t.integer  "primary_contact_id"
+    t.string   "notes"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  add_index "table_financial_alternatives", ["user_id"], name: "index_table_financial_alternatives_on_user_id", using: :btree
 
   create_table "tax_year_infos", force: :cascade do |t|
     t.integer  "year"
@@ -546,6 +567,7 @@ ActiveRecord::Schema.define(version: 20170124074414) do
   add_foreign_key "financial_alternatives", "users"
   add_foreign_key "financial_investments", "users"
   add_foreign_key "shares", "users"
+  add_foreign_key "table_financial_alternatives", "users"
   add_foreign_key "tax_year_infos", "users"
   add_foreign_key "uploads", "users"
   add_foreign_key "user_activities", "users"

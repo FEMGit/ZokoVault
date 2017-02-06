c0bc0047a89e11843c87282d01fd0fbdec2d690d
rzudin <r.zudin@velvetech.ru>
2017-02-05 22:38:58 -0500

    Ad-753 - Fix financial shared issue

--- app/policies/financial_investment_policy.rb
+++ app/policies/financial_investment_policy.rb
@@ -9,6 +9,12 @@ class FinancialInvestmentPolicy < CategorySharePolicy
   def scope
     Pundit.policy_scope!(user, record.class)
   end
+  
+  def owner_shared_record_with_user?
+    return false if record.empty_provider_id.nil?
+    provider = FinancialProvider.find_by(id: record.empty_provider_id)
+    Share.exists?(shareable: provider, contact: Contact.where(emailaddress: user.email))
+  end
 
   class Scope
     attr_reader :user, :scope

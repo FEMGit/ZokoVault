0018ffbbba9c7ad1bb719a27156f7426d8603a70
rzudin <r.zudin@velvetech.ru>
2017-02-21 11:19:24 -0500

    Ad-828 - Email Validation for contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,9 +10,8 @@ class Contact < ActiveRecord::Base
 
   validates :emailaddress, presence: { :message => "Required" }
   validates_uniqueness_of :emailaddress, :scope => [:user_id, :emailaddress], allow_blank: true, message: "Email Address already taken"
-  validates_format_of :emailaddress,
-                      :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
-                      :message => "Email should contain @ and domain like '.com'"
+
+  validate :email_is_valid?
   
   validates_length_of :firstname, :maximum => ApplicationController.helpers.get_max_length(:default)
   validates_length_of :lastname, :maximum => ApplicationController.helpers.get_max_length(:default)
@@ -60,6 +59,20 @@ class Contact < ActiveRecord::Base
     'Advisor' => RELATIONSHIP_TYPES[:professional],
     'Medical Professional' => RELATIONSHIP_TYPES[:professional]
   }
+  
+  def email_is_valid?
+    unless emailaddress =~ MailService.mail_regexp && check_domain(emailaddress)
+      errors.add(:emailaddress, "Email should contain @ and domain like '.com")
+    end
+  end
+  
+  def check_domain(email)
+    email_domain = MailService.email_domain(email)
+    if EmailDomains::DOMAINS.any? { |dom| email_domain =~ dom }
+      return true
+    end
+    false
+  end
 
   def canonical_user
     User.find_by(email: emailaddress)

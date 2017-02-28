a13ea30043481130b4535091dd6b36e111f9b7e7
rzudin <r.zudin@velvetech.ru>
2017-02-27 23:49:53 -0500

    Ad-863- Fix Taxes Save bug

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -12,4 +12,21 @@ class Tax < ActiveRecord::Base
   before_save { self.category = Category.fetch("taxes") }
   
   validates_length_of :notes, :maximum => ApplicationController.helpers.get_max_length(:notes)
+  
+  before_validation :build_shares
+
+  def share_with_contact_ids
+    @share_with_contact_ids || shares.map(&:contact_id)
+  end
+
+  attr_writer :share_with_contact_ids
+  
+  private
+  
+  def build_shares
+    shares.clear
+    share_with_contact_ids.select(&:present?).each do |contact_id|
+      shares.build(user_id: user_id, contact_id: contact_id)
+    end
+  end
 end

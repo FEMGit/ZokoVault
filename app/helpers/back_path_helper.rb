61df62b0644189626bf5964eccade84b3e9d7dde
rzudin <r.zudin@velvetech.ru>
2017-03-22 02:07:14 -0400

    Ad-943 - Fix Flash message

--- app/helpers/back_path_helper.rb
+++ app/helpers/back_path_helper.rb
@@ -3,7 +3,7 @@ module BackPathHelper
     previous_url = previous_path || request.referrer
     return nil unless previous_url.present?
 
-    return root_path if previous_url.eql? root_url
+    return root_path if (previous_url.eql? root_url) || (previous_url.eql? new_user_session_url)
     recognized_path = Rails.application.routes.recognize_path(previous_url)
     return nil if recognized_path[:action] == "catch_404"
     return root_path if previous_url.eql? request.original_url

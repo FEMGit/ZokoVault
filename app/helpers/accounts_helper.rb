a8d52dd124bda1e7d068ad9b347dfaca7e9074be
rzudin <r.zudin@velvetech.ru>
2017-04-25 01:29:48 -0400

    ZV-156 - Trial Membership Ended Template Update

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,4 +2,29 @@ module AccountsHelper
   def radio_button_checked?(value, mfa_frequency)
     return "checked" if mfa_frequency == value
   end
+  
+  def family_members_count
+    return 0 unless current_user
+    Contact.for_user(current_user).where(contact_type: 'Family & Beneficiaries').count
+  end
+  
+  def financial_properties_count
+    return 0 unless current_user
+    FinancialProperty.for_user(current_user).count
+  end
+  
+  def insurance_vendors_count
+    return 0 unless current_user
+    Vendor.for_user(current_user).count
+  end
+  
+  def documents_count
+    return 0 unless current_user
+    Document.for_user(current_user).count
+  end
+  
+  def account_data_any?
+    family_members_count > 0 || financial_properties_count > 0 || 
+      insurance_vendors_count > 0 || documents_count > 0
+  end
 end

402f745ed4308bc9cf2bba7edadd7935aa6374ae
Laura Cordoba <castelinolaura@gmail.com>
2017-04-27 18:59:47 -0300

    [ZV-134] FIX: titleize for Tutorials name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < HighVoltage::PagesController
     @page_number   = params[:page_id]
     @page_name     = "page_#{@page_number}"
     @tutorial_name = params[:tutorial_id]
-    @tutorial      = Tutorial.find_by(name: @tutorial_name.split("-").join(" ").humanize)
+    @tutorial      = Tutorial.find_by(name: @tutorial_name.split("-").join(" ").titleize)
 
     # For Add Primary Contact view
     @primary_contacts = Contact.for_user(current_user).where(relationship: Contact::CONTACT_TYPES['Family & Beneficiaries'], contact_type: 'Family & Beneficiaries')

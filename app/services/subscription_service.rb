fc24c6c434a3196d0acf2038faf250e55dcc0ca3
David Feldman <dbfeldman@gmail.com>
2017-05-02 17:22:10 -0500

    trial setup

--- app/services/subscription_service.rb
+++ app/services/subscription_service.rb
@@ -8,7 +8,24 @@ class SubscriptionService
     user_sub.funding.details = { "stripe_subscription_id" => stripe_sub.id }
     user_sub.save!
     CurrentUserSubscriptionMarker.set_for(
+      user_id: user_sub.user.id, user_subscription_id: user_sub.id)
+    user_sub
+  end
+
+  def self.activate_trial(user:, duration:)
+    user_sub = UserSubscription.new
+    user_sub.user = user
+    user_sub.start_at = Time.current
+    user_sub.end_at = user_sub.start_at + duration
+    user_sub.build_funding(method: "trial", details: {})
+    user_sub.save!
+    CurrentUserSubscriptionMarker.set_for(
       user_id: user.id, user_subscription_id: user_sub.id)
     user_sub
+  end
+
+  def self.trial_was_used?(user)
+    UserSubscription.includes(:funding) \
+                    .where(user: user).map(&:funding).any?(&:trial?)
   end
 end

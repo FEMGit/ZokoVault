f7d6a868c7a8aded60e466c6f7fcab1635ac9406
Roman <rockalocular@MacBook-Pro-fff.Dlink>
2017-05-06 00:58:35 +0700

    ZV-214 - Payment Nan Replace

--- vendor/assets/javascripts/promotion_logic.js
+++ vendor/assets/javascripts/promotion_logic.js
@@ -72,7 +72,12 @@ function updateSubscription() {
     var description = "This amount will be billed to your account on " + billedData;
     $('#subscription-description').text(description);
 
-    var price = '$ ' + (subscriptions[select]['amount'] / 100.0).toFixed(2);
+    var priceValue = (subscriptions[select]['amount'] / 100.0).toFixed(2)
+    if (isNaN(priceValue)) {
+      priceValue = '--'
+    }
+
+    var price = '$ ' + priceValue
     $('#subscription-price').text(price);
     $('#subscription-total').text(price);
     clearPromoRow();

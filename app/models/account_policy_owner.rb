cc5f709ea075cef592f4640d39e2f067a78f5a3e
rzudin <r.zudin@velvetech.ru>
2017-05-11 04:25:26 -0400

    Zv-254 - Fix Account Policy Form Error

--- app/models/account_policy_owner.rb
+++ app/models/account_policy_owner.rb
@@ -3,7 +3,7 @@ class AccountPolicyOwner < ActiveRecord::Base
   belongs_to :card_document
   
   def name
-    (Contact.find_by(id: contact_id) || CardDocument.find_by(id: card_document_id)).name
+    (Contact.find_by(id: contact_id) || CardDocument.find_by(id: card_document_id)).try(:name)
   end
   
   def emailaddress

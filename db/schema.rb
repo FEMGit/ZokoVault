516bc9cbce9a3b845d5b228d285443fb7fcd6e4d
rzudin <r.zudin@velvetech.ru>
2017-05-14 23:37:52 -0400

    ZV-269 - Update tutorials/new page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511070938) do
+ActiveRecord::Schema.define(version: 20170512075831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

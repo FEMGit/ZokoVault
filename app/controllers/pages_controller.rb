987f65b015456471bcb8b0513d2a6d1fe03d7511
Laura Cordoba <castelinolaura@gmail.com>
2017-05-16 11:35:13 -0300

    Update Tutorials UI Update Tutorials names Add slug field to Tutorials and to get correct Tutorial urls

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,14 @@ class PagesController < HighVoltage::PagesController
   def confirmation; end
 
   def show
+    # Params = {"tutorial_id"=>"insurance", "page_id"=>"1"}
+    @page_number   = params[:page_id]
+    @tutorial_name = params[:tutorial_id]
+    @tutorial      = Tutorial.find_by(slug: @tutorial_name.split("-").join(" ").titleize)
+
+    # For Add Primary Contact view
+    @primary_contacts = Contact.for_user(current_user).where(relationship: Contact::CONTACT_TYPES['Family & Beneficiaries'], contact_type: 'Family & Beneficiaries')
+    @contact = Contact.new(user: current_user)
     @show_footer = false
     tuto_index = session[:tutorial_index] + 1
 
@@ -16,7 +24,7 @@ class PagesController < HighVoltage::PagesController
     end
 
     # @next_tutorial_name = session[:tutorial_paths][tuto_index][:tuto_name]
-    @next_tutorial = Tutorial.find_by(name: @next_tutorial_name.titleize)
+    @next_tutorial = Tutorial.find_by(slug: @next_tutorial_name.titleize)
     @next_page = session[:tutorial_paths][tuto_index][:current_page]
     @page_name     = "page_#{@page_number}"
 
@@ -24,20 +32,20 @@ class PagesController < HighVoltage::PagesController
   end
 
   private
-  
+
   def set_cache_headers
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
-  
+
   def set_tutorial
     # Params = {"tutorial_id"=>"insurance", "page_id"=>"1"}
     @page_number   = params[:page_id]
     @tutorial_name = params[:tutorial_id]
     @tutorial      = Tutorial.find_by(name: @tutorial_name.split("-").join(" ").titleize)
   end
-  
+
   def set_contacts
     if @tutorial_name.include? 'primary-contact'
       @primary_contacts = Contact.for_user(current_user).where(relationship: Contact::CONTACT_TYPES['Family & Beneficiaries'], contact_type: 'Family & Beneficiaries')
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516073028) do
+ActiveRecord::Schema.define(version: 20170516135723) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
@@ -545,6 +546,7 @@ ActiveRecord::Schema.define(version: 20170516073028) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "number_of_pages"
+    t.string   "slug"
   end
 
   create_table "user_activities", force: :cascade do |t|

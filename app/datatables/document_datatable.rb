76d1b710c27f49c13852257138ba95f9f688d695
rzudin <r.zudin@velvetech.ru>
2017-06-29 05:33:31 -0400

    ZV-466 - Cotninue

--- app/datatables/document_datatable.rb
+++ app/datatables/document_datatable.rb
@@ -1,4 +1,8 @@
 class DocumentDatatable
+  include Rails.application.routes.url_helpers
+  include DocumentsHelper
+  include SharedViewHelper
+  include ActionView::Helpers::TagHelper
   delegate :root_url, :params, :link_to, :current_user, to: :@view
 
   def initialize(view)
@@ -41,23 +45,21 @@ class DocumentDatatable
   end
   
   def data
-    paginated_records.map do |info|
+    paginated_records.map do |document|
       [
-        traffic_user(info),
-        info.created_at.utc.to_s,
-        traffic_page(info),
-        traffic_url(info),
-        info.ip_address
+        document_link(document),
+        document.updated_at,
+        document_tag(document)
       ]
     end
   end
 
   def get_raw_records
-    Document.for_user(current_user).where(filter).each { |d| authorize d }.order(sort_column + sort_direction)
+    DocumentPolicy::Scope.new(current_user, Document).resolve.where(filter).order(sort_column + sort_direction)
   end
 
   def paginated_records
-    get_raw_records.page(page).per(per_page).order(:created_at)
+    get_raw_records.page(page).per(per_page)
   end
 
   def filter
@@ -79,16 +81,40 @@ class DocumentDatatable
     filters.join(" AND ")
   end
   
-  def join_query
-    "LEFT OUTER JOIN user_profiles AS profiles ON profiles.user_id = " +
-      "user_traffics.shared_user_id OR profiles.user_id = user_traffics.user_id"
-  end
-
   def page
     params[:start].to_i/per_page + 1
   end
 
   def per_page
     params[:length].to_i > 0 ? params[:length].to_i : 10
+  end
+  
+  def document_link(document)
+    if @shared_user.present?
+      link_to document.name, shared_documents_path(@shared_user, document)
+    else
+      link_to document.name, document_path(document)
+    end
+  end
+  
+  def document_tag(document)
+    if document.is_a? Document
+      if document_category(document).nil?
+        content_tag(:span, empty_group_category, class: 'doc-tag')
+      else
+        primary_tag = content_tag(:span, document_category(document), class: 'doc-tag')
+        secondary_tag = 
+          if subcategory_shared?(document.user, current_user, document)
+            if document_name_tag(document)
+              content_tag(:span, document_name_tag(document), class: 'doc-tag secondary-tag')
+            elsif document_group(document)
+              content_tag(:span, document_group(document), class: 'doc-tag secondary-tag')
+            else
+              nil
+            end
+          end
+        primary_tag + secondary_tag
+      end
+    end
   end
 end

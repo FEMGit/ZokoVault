05b4af2c6351897295c56270a4132b6295b364cd
rzudin <r.zudin@velvetech.ru>
2017-06-30 03:11:59 -0400

    ZV-481 - Corporate Email Updates

--- app/mailers/share_invitation_mailer.rb
+++ app/mailers/share_invitation_mailer.rb
@@ -22,6 +22,6 @@ class ShareInvitationMailer < ApplicationMailer
     @user.confirmed_at = Time.now
     @user.save(validate: false)
     @corporate_admin = corporate_admin
-    mail(to: @contact.emailaddress, subject: "#{corporate_admin.name} created an account for you on ZokuVault!", :cc => corporate_admin.email)
+    mail(to: @contact.emailaddress, subject: "#{corporate_admin.name} created an account for you on ZokuVault!")
   end
 end

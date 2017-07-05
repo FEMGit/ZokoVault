fe983b5d7bb779e6c15acec7c68992b2aae0d8fd
rzudin <r.zudin@velvetech.ru>
2017-07-05 00:27:58 -0400

    ZV-509 - Email Preview Issue Fix

--- app/assets/javascripts/email_preview.js
+++ app/assets/javascripts/email_preview.js
@@ -12,18 +12,38 @@ var setEmailPreviewMultipleDropdown = function(chosenSelector) {
 
 var addRemoveShareInfoLineSingleDropdown = function(e, params) {
   $("#email-preview-list").find("p").remove()
-  if (params === undefined) {
-    return
-  } else if (params["selected"] !== undefined) {
-    appendNewEmailPreviewLine(params["selected"])
-  }
+  var targetId = $(e.target).attr('id')
+  
+  $.each($("#" + targetId + " option:selected"), function(i, val) {
+    appendNewEmailPreviewLine(val.value)
+  })
 }
 
 var addRemoveShareInfoLineMultipleDropdown = function(e, params) {
-  if(params["deselected"] !== undefined) {
-    $("#share-contact-message-" + params["deselected"]).remove()
-  } else if (params["selected"] !== undefined) {
-    appendNewEmailPreviewLine(params["selected"])
+  var targetId = $(e.target).attr('id')
+  var oldIds = []
+  // Get ids for current email preview messages
+  $.each($("[id^='share-contact-message']"), function(i, val) {
+    var newId = val.id.match(/\d+/)[0]
+    if (oldIds.indexOf(newId) < 0) {
+      oldIds.push(newId)
+    }
+  })
+
+  // Add messages for ids that are not in 'oldIds'
+  $.each($("#" + targetId + " option:selected"), function(i, val) {
+    var index = oldIds.indexOf(val.value)
+    if (index >= 0) {
+      oldIds.splice(index, 1)
+      return true
+    } else {
+      appendNewEmailPreviewLine(val.value)
+    }
+  })
+  
+  // Remove messages with all unused 'oldIds' items
+  for(var i = 0; i < oldIds.length; i++) {
+    $("#share-contact-message-" + oldIds[i]).remove()
   }
 }
 

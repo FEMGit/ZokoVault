c56f2cf1ffac3c7d2f82e749c5daafaeafcae46c
rzudin <r.zudin@velvetech.ru>
2017-08-16 00:24:31 -0400

    ZV-618 - My Profile Fixes

--- app/policies/user_profile_policy.rb
+++ app/policies/user_profile_policy.rb
@@ -1,7 +1,17 @@
 class UserProfilePolicy < BasicPolicy
-
+  def index?
+    user_owned?
+  end
+  
+  def edit?
+    user_owned?
+  end
+  
+  def update?
+    user_owned?
+  end
+  
   def set_user_profile?
     user_owned?
   end
-
 end

cd8be8d38f546a9a2fe51e3843272dda8deee2fc
rzudin <r.zudin@velvetech.ru>
2017-08-21 23:26:56 -0400

    ZV-636 - Corporate Billing Information Invoices

--- app/helpers/stripe_helper.rb
+++ app/helpers/stripe_helper.rb
@@ -4,4 +4,13 @@ module StripeHelper
   rescue Stripe::InvalidRequestError, Stripe::CardError => se
     on_failure && on_failure.call(se)
   end
+  
+  def user_name_by_subscription_id(subscription_id)
+    if stripe_subscription = StripeSubscription.find_by(:subscription_id => subscription_id)
+      return '--' unless user = stripe_subscription.user
+      user.name
+    else
+      '--'
+    end
+  end
 end

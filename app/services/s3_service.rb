b31c2c1007d3c2b266f1f17b815687a4d5bc9a6b
David Feldman <dbfeldman@gmail.com>
2017-08-29 18:57:00 -0500

    refactor AWS IAM logic

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -1,19 +1,22 @@
+require 'zoku/aws/iam'
+
 class S3Service
-  def self.get_object_by_key(document_key)
-    Aws::S3::Object.new(Rails.application.secrets.aws_bucket_name, document_key, region: Rails.application.secrets.aws_region, credentials: assume_role_credentials)
-  end
-  
-  def self.delete_from_storage(document_key, options = {})
-    client = Aws::S3::Client.new(region: Rails.application.secrets.aws_region, credentials: assume_role_credentials)
-    client.delete_object(options.merge(:bucket => Rails.application.secrets.aws_bucket_name, :key => document_key))
-  end
-  
-  def self.assume_role_credentials
-    sts = Aws::STS::Client.new(region: Rails.application.secrets.aws_region, credentials: user_credentials)
-    Aws::AssumeRoleCredentials.new(client: sts, role_arn: Rails.application.secrets.aws_access_role, region: Rails.application.secrets.aws_region, role_session_name: 'S3Access')
+  def self.get_object_by_key(document_key, bucket: nil)
+    bucket ||= Rails.application.secrets.aws_bucket_name
+    Aws::S3::Object.new(
+      bucket,
+      document_key,
+      region: Zoku::AWS::IAM.region,
+      credentials: Zoku::AWS::IAM.s3_role
+    )
   end
 
-  def self.user_credentials
-    Aws::Credentials.new(Rails.application.secrets.aws_user_key_id, Rails.application.secrets.aws_user_secret_key)
+  def self.delete_from_storage(document_key, **options)
+    options[:bucket] ||= Rails.application.secrets.aws_bucket_name
+    client = Aws::S3::Client.new(
+      region: Zoku::AWS::IAM.region,
+      credentials: Zoku::AWS::IAM.s3_role
+    )
+    client.delete_object(options.merge(key: document_key))
   end
 end
---
+++ lib/zoku/aws/iam.rb
@@ -0,0 +1,49 @@
+module Zoku
+  module AWS
+    module IAM
+
+      def self.region
+        Rails.application.secrets.aws_region
+      end
+
+      def self.s3_user
+        Aws::Credentials.new(
+          Rails.application.secrets.aws_user_key_id,
+          Rails.application.secrets.aws_user_secret_key
+        )
+      end
+
+      def self.s3_role
+        assume_role(
+          user:       s3_user,
+          role_arn:   'arn:aws:iam::751141858228:role/s3access',
+          role_alias: 'S3Access',
+          region:     region
+        )
+      end
+
+      def self.kms_user
+        Aws::Credentials.new(
+          Rails.application.secrets.aws_crypto_user_key_id,
+          Rails.application.secrets.aws_crypto_user_secret_key
+        )
+      end
+
+      def self.kms_role
+        assume_role(
+          user:       kms_user,
+          role_arn:   'arn:aws:iam::751141858228:role/per-user-crypto-role',
+          role_alias: 'PerUserCryptoKeys',
+          region:     region
+        )
+      end
+
+      def self.assume_role(user:, role_arn:, role_alias:, region:)
+        client = Aws::STS::Client.new(region: region, credentials: user)
+        Aws::AssumeRoleCredentials.new(
+          client: client, role_arn: role_arn, role_session_name: role_alias)
+      end
+
+    end
+  end
+end

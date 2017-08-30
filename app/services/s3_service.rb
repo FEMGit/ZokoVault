982b8272722ca78b3a923010dd42981775375ba1
David Feldman <dbfeldman@gmail.com>
2017-08-29 19:29:38 -0500

    simplify

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -1,22 +1,16 @@
-require 'zoku/aws/iam'
+require 'zoku/aws/s3'
 
 class S3Service
-  def self.get_object_by_key(document_key, bucket: nil)
-    bucket ||= Rails.application.secrets.aws_bucket_name
+  def self.get_object_by_key(document_key)
     Aws::S3::Object.new(
-      bucket,
-      document_key,
-      region: Zoku::AWS::IAM.region,
-      credentials: Zoku::AWS::IAM.s3_role
+      bucket_name: Zoku::AWS::S3.bucket_name,
+      key: document_key,
+      client: Zoku::AWS::S3.client
     )
   end
 
   def self.delete_from_storage(document_key, **options)
-    options[:bucket] ||= Rails.application.secrets.aws_bucket_name
-    client = Aws::S3::Client.new(
-      region: Zoku::AWS::IAM.region,
-      credentials: Zoku::AWS::IAM.s3_role
-    )
-    client.delete_object(options.merge(key: document_key))
+    Zoku::AWS::S3.client.delete_object(
+      options.merge(key: document_key, bucket: Zoku::AWS::S3.bucket_name))
   end
 end

e852ebfea7289c6dcb25aa31d3a27bc8c0609048
David Feldman <dbfeldman@gmail.com>
2017-09-07 01:29:40 -0500

    cleanup StagingHelper

--- app/helpers/staging_helper.rb
+++ app/helpers/staging_helper.rb
@@ -1,7 +1,32 @@
 module StagingHelper
+  extend self
+  
+  TYPES = {
+    local:        'local'.freeze,
+    development:  'develop'.freeze,
+    staging:      'beta'.freeze,
+    production:   'production'.freeze
+  }.freeze
+  
+  def staging_type
+    ENV.fetch('STAGING_TYPE', TYPES[:local])
+  end
+  
+  def known_environment?
+    TYPES.values.include?(staging_type)
+  end
+  
+  def heroku_app_name
+    ENV.fetch('HEROKU_APP_NAME', '')
+  end
+  
+  def heroku_review_app?
+    !!(heroku_app_name =~ /\Azoku-develop/)
+  end
+  
   def develop_staging?
-    staging_type = ENV['STAGING_TYPE'] || nil
-    ((ENV['HEROKU_APP_NAME'] || '').include? 'zoku-develop') ||
-      staging_type.blank? || (staging_type.eql? 'develop')
+    heroku_review_app? ||
+    staging_type == TYPES[:local] ||
+    staging_type == TYPES[:development]
   end
 end

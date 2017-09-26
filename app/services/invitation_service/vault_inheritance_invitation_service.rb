cb9f3a8d561648733f03cec874662ae286cec276
rzudin <r.zudin@velvetech.ru>
2017-09-26 05:43:48 -0400

    ZV-758 - Add Contingent Owner Setup Record To Accout Traffic

--- app/services/invitation_service/vault_inheritance_invitation_service.rb
+++ app/services/invitation_service/vault_inheritance_invitation_service.rb
@@ -1,16 +1,20 @@
 module InvitationService
   module VaultInheritanceInvitationService
-    def self.send_invitation(user:, contact:)
-      return unless contact.present? && user.present?
+    class << self
+      include Rails.application.routes.url_helpers
       
-      invitation_args =
-        if (user_contact = User.where('email ILIKE ?', contact.try(:emailaddress)).first).present?
-          [:existing_user, user_contact, user]
-        else
-          [:new_user, contact, user]
-        end
+      def send_invitation(user:, contact:)
+        return unless contact.present? && user.present?
 
-      VaultInheritanceInvitationMailer.send(*invitation_args).deliver_now
+        invitation_args =
+          if (user_contact = User.where('email ILIKE ?', contact.try(:emailaddress)).first).present?
+            [:existing_user, user_contact, user]
+          else
+            [:new_user, contact, user]
+          end
+        VaultInheritanceInvitationMailer.send(*invitation_args).deliver_now
+        UserTrafficModule.save_traffic_with_params(page_url: contact_path(contact), page_name: "Designated #{contact.name} as contingent owner", user: user, ip_address: '--')
+      end
     end
   end
 end

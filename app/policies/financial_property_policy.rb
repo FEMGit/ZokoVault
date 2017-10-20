698a797123910cefb3afd80a9a2f02379f4b364c
rzudin <r.zudin@velvetech.ru>
2017-10-20 01:06:29 -0400

    ZV-831 - Balance Sheet Shared View Access Fix

--- app/policies/financial_property_policy.rb
+++ app/policies/financial_property_policy.rb
@@ -1,9 +1,15 @@
 class FinancialPropertyPolicy < CategorySharePolicy
-
+  def update_balance_sheet?
+    owned_or_shared?
+  end
+  
+  def balance_sheet?
+    owned_or_shared?
+  end
+  
   def owner_shared_record_with_user?
     return false if record.nil? || record.empty_provider_id.nil?
     provider = FinancialProvider.find_by(id: record.empty_provider_id)
     Share.exists?(shareable: provider, contact: Contact.where("emailaddress ILIKE ?", user.email))
   end
-
 end

6d24ae96889a361c9025ec4006de51a30a2cb947
David Feldman <dbfeldman@gmail.com>
2017-10-24 16:30:04 -0500

    simplify service

--- app/services/contact_service.rb
+++ app/services/contact_service.rb
@@ -1,30 +1,28 @@
 class ContactService
-  attr_reader :contacts
+  attr_reader :user
+  
+  def self.filter_contacts(contact_ids)
+    cids = contact_ids.flatten.uniq
+    Contact.where(id: cids).pluck(:id)
+  end
 
-  def initialize(params)
-    @user = params[:user]
-    set_contacts
+  def initialize(user:)
+    @user = user
   end
 
   def contacts_shareable
-    @contacts.reject { |c| contact_all_emails_rejected.include? c.emailaddress }
-  end
-  
-  def self.filter_contacts(contact_ids)
-    Contact.pluck(:id).select { |x| contact_ids.include? x }
+    contacts.reject { |c| contact_all_emails_rejected.include?(c.emailaddress) }
   end
   
   def contact_corporate_emails_rejected
-    @user.corporate_users.try(:map, &:email).try(:map, &:downcase)
+    user.corporate_users.try(:map, &:email).try(:map, &:downcase) || []
   end
   
   def contact_all_emails_rejected
-    @user.corporate_users.try(:map, &:email).try(:map, &:downcase) << @user.email.downcase
+    contact_corporate_emails_rejected << user.email.downcase
   end
   
-  private
-
-  def set_contacts
-    @contacts = Contact.for_user(@user).reject { |c| contact_corporate_emails_rejected.include? c.emailaddress }
+  def contacts
+    @contacts ||= user.contacts.reject { |c| contact_corporate_emails_rejected.include?(c.emailaddress) }
   end
 end
